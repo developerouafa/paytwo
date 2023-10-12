71edbae9e07b7ad32f656c59b7ae6363b8b6f2ad
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-10-12 10:15:05 +0100

    Table|Model FundAccount

---
+++ app/Http/Controllers/FundAccountController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\fund_account;
+use Illuminate\Http\Request;
+
+class FundAccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(fund_account $fund_account)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(fund_account $fund_account)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, fund_account $fund_account)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(fund_account $fund_account)
+    {
+        //
+    }
+}

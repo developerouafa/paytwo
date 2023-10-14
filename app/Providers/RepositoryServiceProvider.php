3eccca9c0b5335a48878e51062fdb77bc765e51a
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-10-14 10:55:21 +0100

    Provider Reciept

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Interfaces\Clients\ClientRepositoryInterface;
 use App\Interfaces\Clients\ProfileclientRepositoryInterface;
 use App\Interfaces\Clients\Profiles\ProfileclientRepositoryInterface as ProfilesProfileclientRepositoryInterface;
+use App\Interfaces\dashboard_user\Finances\ReceiptRepositoryInterface;
 use App\Interfaces\Products\mainRepositoryInterface;
 use App\Interfaces\Products\multipeRepositoryInterface;
 use App\Interfaces\Sections\childrenRepositoryInterface;
@@ -14,6 +15,7 @@ use App\Interfaces\Products\promotionRepositoryInterface;
 use App\Interfaces\Products\stockRepositoryInterface;
 use App\Repository\Clients\ClientRepository;
 use App\Repository\Clients\Profiles\ProfileclientRepository;
+use App\Repository\Finance\ReceiptRepository;
 use App\Repository\Products\mainimageRepository;
 use App\Repository\Products\multipimageRepository;
 use App\Repository\Sections\childrenRepository;
@@ -40,6 +42,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(multipeRepositoryInterface::class, multipimageRepository::class);
         $this->app->bind(ClientRepositoryInterface::class, ClientRepository::class);
         $this->app->bind(ProfilesProfileclientRepositoryInterface::class, ProfileclientRepository::class);
+        $this->app->bind(ReceiptRepositoryInterface::class, ReceiptRepository::class);
     }
 
     /**

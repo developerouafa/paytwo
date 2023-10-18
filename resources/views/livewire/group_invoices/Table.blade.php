bac79efe753ce85fd4d02946c7ca7aee8ca8c40a
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-10-18 13:01:41 +0100

    Complet deletegroup & Excel pdf.. In groupinvoices

--- resources/views/livewire/group_invoices/Table.blade.php
+++ resources/views/livewire/group_invoices/Table.blade.php
@@ -1,6 +1,6 @@
 <button class="btn btn-primary pull-right" wire:click="show_form_add" type="button"> {{__('Dashboard/services.addotheinvoice')}} </button><br><br>
 <div class="table-responsive">
-    <table class="table text-md-nowrap" id="example1" data-page-length="50"style="text-align: center">
+    <table id="example" class="table key-buttons text-md-nowrap">
         <thead>
             <tr>
                 <th>#</th>

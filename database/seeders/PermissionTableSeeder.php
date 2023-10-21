cd299794d4f72899d56eb3a1b161c2a42f46f5b5
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-10-21 09:26:50 +0100

    permission user-sidebar @can @endcan

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -14,6 +14,21 @@ class PermissionTableSeeder extends Seeder
     public function run(): void
     {
         $permissions = [
+            'sidebar users & permissions',
+            'sidebar users',
+            'sidebar permissions',
+            'sidebar Section & Children',
+            'sidebar section',
+            'sidebar children',
+            'sidebar products',
+            'sidebar clients',
+            'sidebar Group Services',
+            'sidebar Singleservice & groupservice',
+            'sidebar Singleservice',
+            'sidebar groupservice',
+            'sidebar receipt & Payment',
+            'sidebar receipt',
+            'sidebar receipt',
             'role-list',
             'role-create',
             'role-edit',

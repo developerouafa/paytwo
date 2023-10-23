a65fe834b07cb0623c5d838cfbae353d6861a2a5
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-10-23 13:28:18 +0100

    Problem In app.js

--- resources/views/Dashboard/layouts/main-header.blade.php
+++ resources/views/Dashboard/layouts/main-header.blade.php
@@ -258,6 +258,7 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
 
+{{-- Problem --}}
 <script src="{{URL::asset('js/app.js')}}"></script>
 
 <script>

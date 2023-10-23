db0fbb6e7ccbe8c611475d059d05b629f5613f01
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-10-23 12:39:50 +0100

    app.js In Sidebar(pusher)

--- resources/views/Dashboard/layouts/main-header.blade.php
+++ resources/views/Dashboard/layouts/main-header.blade.php
@@ -258,6 +258,8 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
 
+<script src="{{URL::asset('js/app.js')}}"></script>
+
 <script>
     var notificationsWrapper   = $('.dropdown-notifications');
     var notificationsCountElem = notificationsWrapper.find('p[data-count]');

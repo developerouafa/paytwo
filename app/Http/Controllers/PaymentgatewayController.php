42f2feb4c579bd33d6663539855c12ebe641dd25
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-10-25 14:58:16 +0100

    PaymentGateways table

---
+++ app/Http/Controllers/PaymentgatewayController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\paymentgateway;
+use Illuminate\Http\Request;
+
+class PaymentgatewayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(paymentgateway $paymentgateway)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(paymentgateway $paymentgateway)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, paymentgateway $paymentgateway)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(paymentgateway $paymentgateway)
+    {
+        //
+    }
+}

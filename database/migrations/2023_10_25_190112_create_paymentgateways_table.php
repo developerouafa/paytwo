79eccf840618ef839e2d4462391974ba9d19607d
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-10-25 15:22:12 +0100

    Complet Add PaymentGateways => SingleInvoices

--- database/migrations/2023_10_25_190112_create_paymentgateways_table.php
+++ database/migrations/2023_10_25_190112_create_paymentgateways_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->date('date');
             $table->foreignId('invoice_id')->nullable()->references('id')->on('invoices')->onDelete('cascade');
             $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('client_id')->references('id')->on('clients')->onDelete('cascade')->onUpdate('cascade');
             $table->decimal('Debit',8,2)->nullable();
             $table->decimal('credit',8,2)->nullable();
             $table->softDeletes();

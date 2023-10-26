a0a9310e0da8ccf12d247bc1edf9c363f2d55282
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-10-26 14:55:57 +0100

    expired delete sections

--- app/Console/Commands/deleteexpired.php
+++ app/Console/Commands/deleteexpired.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\section;
 use Illuminate\Console\Command;
 
 class deleteexpired extends Command
@@ -11,20 +12,23 @@ class deleteexpired extends Command
      *
      * @var string
      */
-    protected $signature = 'app:deleteexpired';
+    protected $signature = 'delete:deleteexpired';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Delete expiry (delete table => delete basket) every expiration date automatically';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $admins = section::where('deleted_at', '>', date('Y-m-d'))->get();
+        foreach($admins as $admin){
+            $admin->delete();
+        }
     }
 }

4eb103db1508fc0579dbb3587bcfeec8cbf032ed
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-10-26 12:19:18 +0100

    Repository Invoice => CLIENT

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Interfaces\Clients\Invoices\InvoiceRepositoryInterface;
 use App\Interfaces\Clients\Profiles\ProfileclientRepositoryInterface;
 use App\Interfaces\dashboard_user\Clients\ClientRepositoryInterface;
 use App\Interfaces\dashboard_user\Finances\PaymentRepositoryInterface;
@@ -13,6 +14,7 @@ use App\Interfaces\dashboard_user\Products\promotionRepositoryInterface;
 use App\Interfaces\dashboard_user\Products\stockRepositoryInterface;
 use App\Interfaces\dashboard_user\Sections\childrenRepositoryInterface;
 use App\Interfaces\dashboard_user\Sections\SectionRepositoryInterface;
+use App\Repository\Clients\Profiles\InvoicesRepository;
 use App\Repository\Clients\Profiles\ProfileclientRepository;
 use App\Repository\dashboard_user\Clients\ClientRepository;
 use App\Repository\dashboard_user\Finances\PaymentRepository;
@@ -44,6 +46,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(ProfileclientRepositoryInterface::class, ProfileclientRepository::class);
         $this->app->bind(ReceiptRepositoryInterface::class, ReceiptRepository::class);
         $this->app->bind(PaymentRepositoryInterface::class, PaymentRepository::class);
+        $this->app->bind(InvoiceRepositoryInterface::class, InvoicesRepository::class);
     }
 
     /**
---
+++ app/Repository/clients/invoices/InvoicesRepository.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Repository\Clients\Profiles;
+
+use App\Interfaces\Clients\Invoices\InvoiceRepositoryInterface;
+
+
+class InvoicesRepository implements InvoiceRepositoryInterface
+{
+
+}

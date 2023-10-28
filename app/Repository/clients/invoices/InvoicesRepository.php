f4aeff72a459857bdf63b3bf65f01ba479bc5593
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-10-28 09:38:13 +0100

    read_at In showinvoice

--- app/Repository/clients/invoices/InvoicesRepository.php
+++ app/Repository/clients/invoices/InvoicesRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository\Clients\Invoices;
 use App\Interfaces\Clients\Invoices\InvoiceRepositoryInterface;
 use App\Models\invoice;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class InvoicesRepository implements InvoiceRepositoryInterface
 {
@@ -25,18 +26,24 @@ class InvoicesRepository implements InvoiceRepositoryInterface
     public function showinvoicemonetary($id)
     {
         $invoice = invoice::latest()->where('type', '1')->where('id', $id)->where('client_id', Auth::user()->id)->first();
+        $getID = DB::table('notifications')->where('data->invoice_id', $id)->pluck('id');
+        DB::table('notifications')->where('id', $getID)->update(['read_at'=>now()]);
         return view('Dashboard.dashboard_client.invoices.showinvoicemonetary', ['invoice' => $invoice]);
     }
 
     public function showinvoicePostpaid($id)
     {
         $invoice = invoice::latest()->where('type', '2')->where('id', $id)->where('client_id', Auth::user()->id)->first();
+        $getID = DB::table('notifications')->where('data->invoice_id', $id)->pluck('id');
+        DB::table('notifications')->where('id', $getID)->update(['read_at'=>now()]);
         return view('Dashboard.dashboard_client.invoices.showinvoicePostpaid', ['invoice' => $invoice]);
     }
 
     public function showinvoiceBanktransfer($id)
     {
         $invoice = invoice::latest()->where('type', '3')->where('id', $id)->where('client_id', Auth::user()->id)->first();
+        $getID = DB::table('notifications')->where('data->invoice_id', $id)->pluck('id');
+        DB::table('notifications')->where('id', $getID)->update(['read_at'=>now()]);
         return view('Dashboard.dashboard_client.invoices.showinvoiceBanktransfer', ['invoice' => $invoice]);
     }
 }

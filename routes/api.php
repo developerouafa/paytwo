452dc65dac234b9b0a4b264dbd82b2970af06753
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-11-18 13:59:40 +0100

    Token Client

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthClientController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +23,14 @@ use Illuminate\Support\Facades\Route;
     Route::group(['middlware' => ['api'], 'namespace' => 'api'], function () {
         Route::group(['prefix' => 'admin'], function(){
             Route::post('login', [AuthController::class, 'login'])->name('login');
-            Route::post('logout', [AuthController::class, 'logout'])->middleware('admintoken:admin_token');
+            // Route::post('logout', [AuthController::class, 'logout'])->middleware('admintoken:admin_token');
+        });
+    });
+
+    // Clients
+    Route::group(['middlware' => ['api'], 'namespace' => 'api'], function () {
+        Route::group(['prefix' => 'jwtclients'], function(){
+            Route::post('login', [AuthClientController::class, 'login'])->name('login');
+            // Route::post('logout', [AuthClientController::class, 'logout'])->middleware('admintoken:admin_token');
         });
     });

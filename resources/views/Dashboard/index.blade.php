14e7e8b697f1141646e3f1fed0818b8b9c5d23c9
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-11-20 10:35:03 +0100

    function invoice complet & success

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -69,28 +69,6 @@
                 </div>
             </div>
             <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
-                <div class="card overflow-hidden sales-card bg-danger-gradient">
-                    <div class="pl-3 pt-3 pr-3 pb-2">
-                        <div class="">
-                            <h6 class="mb-3 tx-12 text-white">TODAY EARNINGS</h6>
-                        </div>
-                        <div class="pb-0 mt-0">
-                            <div class="d-flex">
-                                <div class="">
-                                    <h4 class="tx-20 font-weight-bold mb-1 text-white">$1,230.17</h4>
-                                    <p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
-                                </div>
-                                <span class="float-right my-auto mr-auto">
-                                    <i class="fas fa-arrow-circle-down text-white"></i>
-                                    <span class="text-white op-7"> -23.09%</span>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <span id="compositeline2" class="pt-1">3,2,4,6,12,14,8,7,14,16,12,7,8,4,3,2,2,5,6,7</span>
-                </div>
-            </div>
-            <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
                 <div class="card overflow-hidden sales-card bg-success-gradient">
                     <div class="pl-3 pt-3 pr-3 pb-2">
                         <div class="">
@@ -112,6 +90,30 @@
                         </div>
                     </div>
                     <span id="compositeline3" class="pt-1">5,10,5,20,22,12,15,18,20,15,8,12,22,5,10,12,22,15,16,10</span>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
+                <div class="card overflow-hidden sales-card bg-danger-gradient">
+                    <div class="pl-3 pt-3 pr-3 pb-2">
+                        <div class="">
+                            <h6 class="mb-3 tx-12 text-white">TODAY EARNINGS</h6>
+                        </div>
+                        <div class="pb-0 mt-0">
+                            <div class="d-flex">
+                                <div class="">
+                                    <h4 class="tx-20 font-weight-bold mb-1 text-white">
+                                        {{ invoice::where('invoice_status', 4)->where('invoice_type', 2)->count() }}
+                                    </h4>
+                                    <p class="mb-0 tx-12 text-white op-7">Compared to last week</p>
+                                </div>
+                                <span class="float-right my-auto mr-auto">
+                                    <i class="fas fa-arrow-circle-down text-white"></i>
+                                    <span class="text-white op-7"> -23.09%</span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <span id="compositeline2" class="pt-1">3,2,4,6,12,14,8,7,14,16,12,7,8,4,3,2,2,5,6,7</span>
                 </div>
             </div>
             <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">

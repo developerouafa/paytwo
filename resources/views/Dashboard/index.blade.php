a0715a7ec5530f82af3012beeb3a71717d0d3a0a
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-11-21 20:26:52 +0100

    code

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -538,38 +538,7 @@
 
     <!-- row opened -->
         <div class="row row-sm row-deck">
-            <div class="col-md-12 col-lg-4 col-xl-4">
-                <div class="card card-dashboard-eight pb-2">
-                    <h6 class="card-title">Your Top Countries</h6><span class="d-block mg-b-10 text-muted tx-12">Sales performance revenue based by country</span>
-                    <div class="list-group">
-                        <div class="list-group-item border-top-0">
-                            <i class="flag-icon flag-icon-us flag-icon-squared"></i>
-                            <p>United States</p><span>$1,671.10</span>
-                        </div>
-                        <div class="list-group-item">
-                            <i class="flag-icon flag-icon-nl flag-icon-squared"></i>
-                            <p>Netherlands</p><span>$1,064.75</span>
-                        </div>
-                        <div class="list-group-item">
-                            <i class="flag-icon flag-icon-gb flag-icon-squared"></i>
-                            <p>United Kingdom</p><span>$1,055.98</span>
-                        </div>
-                        <div class="list-group-item">
-                            <i class="flag-icon flag-icon-ca flag-icon-squared"></i>
-                            <p>Canada</p><span>$1,045.49</span>
-                        </div>
-                        <div class="list-group-item">
-                            <i class="flag-icon flag-icon-in flag-icon-squared"></i>
-                            <p>India</p><span>$1,930.12</span>
-                        </div>
-                        <div class="list-group-item border-bottom-0 mb-0">
-                            <i class="flag-icon flag-icon-au flag-icon-squared"></i>
-                            <p>Australia</p><span>$1,042.00</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-12 col-lg-8 col-xl-8">
+            <div class="col-lg-12">
                 <div class="card card-table-two">
                     <div class="d-flex justify-content-between">
                         <h4 class="card-title mb-1">Your Most Recent Earnings</h4>
@@ -588,7 +557,7 @@
                             </thead>
                             <tbody>
                                 <tr>
-                                    {{client_account::whereYear('created_at', now()->format('Y'))->whereMonth('created_at', '=', '1')->orWhereMonth('created_at', '=', '2')->orWhereMonth('created_at', '=', '3')->get()}}
+                                    {{-- {{client_account::whereYear('created_at', now()->format('Y'))->whereMonth('created_at', '=', '1')->orWhereMonth('created_at', '=', '2')->orWhereMonth('created_at', '=', '3')->get()}} --}}
                                     <td>05 Dec 2019</td>
                                     <td class="tx-right tx-medium tx-inverse">34</td>
                                     <td class="tx-right tx-medium tx-inverse">$658.20</td>

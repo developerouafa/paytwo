5ce03c0705a6152957007f8d752170a0d1fe0d73
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-11-23 19:38:13 +0100

    groupproduct in deleteexpired

--- app/Console/Commands/deleteexpired.php
+++ app/Console/Commands/deleteexpired.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Client;
+use App\Models\groupprodcut;
 use App\Models\invoice;
 use App\Models\paymentaccount;
 use App\Models\product;
@@ -39,5 +40,6 @@ class deleteexpired extends Command
         receipt_account::where('deleted_at', '<=', now()->subDays( 30 ))->forcedelete();
         paymentaccount::where('deleted_at', '<=', now()->subDays( 30 ))->forcedelete();
         invoice::where('deleted_at', '<=', now()->subDays( 30 ))->forcedelete();
+        groupprodcut::where('deleted_at', '<=', now()->subDays( 30 ))->forcedelete();
     }
 }

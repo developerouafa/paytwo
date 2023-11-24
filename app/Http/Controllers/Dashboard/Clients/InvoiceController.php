953ce55680ec54046de858bd6e04b82102ed31be
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-11-24 22:16:10 +0100

    link interface to cpanel

--- app/Http/Controllers/Dashboard/Clients/InvoiceController.php
+++ app/Http/Controllers/Dashboard/Clients/InvoiceController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Dashboard\Clients;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Clients\profiles\CompletedRequest;
-use App\Interfaces\Clients\invoices\InvoiceRepositoryInterface;
+use App\Interfaces\clients\invoices\InvoiceRepositoryInterface;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller

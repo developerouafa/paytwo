8a05a241dfd730ab79fb005d83909a905df7fa4f
Ouafa <92248542+developerouafa@users.noreply.github.com>
2023-11-25 15:03:29 +0100

    style

--- resources/views/Dashboard/dashboard_user/childrens/showproduct.blade.php
+++ resources/views/Dashboard/dashboard_user/childrens/showproduct.blade.php
@@ -13,7 +13,8 @@
     <div class="breadcrumb-header justify-content-between">
         <div class="my-auto">
             <div class="d-flex">
-                <h4 class="content-title mb-0 my-auto">{{$section->name}}</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ {{trans('Dashboard/products.section_products')}}</span>
+                <h4 class="content-title mb-0 my-auto">{{$section->name}}</h4>
+                <span class="text-muted mt-1 tx-13 mr-2 mb-0">/ {{trans('Dashboard/products.section_products')}}</span>
             </div>
         </div>
     </div>
